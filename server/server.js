8a8fe1cd60f56a9bbf1bdf6f899601de4c58dc47
André Santos <andre@dexdev.com.br>
2018-01-16 00:25:13 -0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -1,31 +1,49 @@
 const express = require('express');
 const path = require('path');
-const http = require('http'); 
+const http = require('http');
 const socketIO = require('socket.io');
-const publicPath = path.join(__dirname, '../public'); 
-const port = process.env.PORT || 3000; 
-const app = express(); 
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
 const server = http.createServer(app);
 
 const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
+io.on('connection', socket => {
+	console.log('New user connected');
 
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+    socket.emit('newMessage', {
+        from: 'Admin', 
+        text: 'Welcome to the chat app', 
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin', 
+        text:'New user joined', 
+        createdAt: new Date().getTime()
     })
-    socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    }); 
-}); 
+
+	socket.on('createMessage', message => {
+		console.log('createMessage', message);
+		// io.emit('newMessage', {
+		//     from: message.from,
+		//     text: message.text,
+		//     createdAt: new Date().getTime()
+		// });
+
+		// socket.broadcast.emit('newmessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime(),
+		// });
+	});
+	socket.on('disconnect', () => {
+		console.log('User was disconnected');
+	});
+});
 server.listen(port, () => {
-    console.log(`App listening on port ${port}!`);
+	console.log(`App listening on port ${port}!`);
 });
